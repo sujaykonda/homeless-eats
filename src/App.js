bf4819290eb46bdb13686c89d9eead3c91bbda83
Adikesh Nathan <88110937+Turtlez200@users.noreply.github.com>
2023-04-15 14:27:18 -0400

    Goofy Ahh commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {useJsApiLoader, GoogleMap, Marker} from "@react-google-maps/api"
+import "./App.css"
+import React, { useEffect } from "react";
+
+const center = { lat: 48.8584, lng: 2.2945 }
+
 
 function App() {
+
+
+  useEffect(() => {   j
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+       
+        center.lat = position.coords.latitude
+        center.lng =  position.coords.longitude
+        
+      });
+    }
+
+    
+  }, []);
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ['places'],
+  })
+
+  if (!isLoaded){
+    return (
+      <div className="App">
+        <header className="App-header">
+         balls
+        </header>
+      </div>
+    );
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div  >
+      <header className="App">
+      
       </header>
+      <div> 
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ height: "100vh"}}
+          
+        />
+      </div>
+
+      
     </div>
   );
 }
